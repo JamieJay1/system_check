379a71df335d300d684e3d7f4ca99772966042da
Olusegun <kml@OLUSEGUN-PC>
2025-04-16 16:56:11 +0100

    Updated system check

--- system_report.sh
+++ system_report.sh
@@ -1,9 +1,16 @@
+
 # !/bin/bash
-echo "==========================================================="
-echo "			System Health Report"
-echo "==========================================================="
-echo "System Check - $(date)"
-echo "Uptime:"
-uptime
-echo "Disk Space:"
-df -h 
+mkdir -p "$HOME/git/sys_check"
+log_file="$HOME/git/sys_check/system_report.log" 
+{
+	echo "==========================================================="
+	echo "			System Health Report"
+	echo "==========================================================="
+
+
+	echo "System Check - $(date)"
+	echo "Uptime:"
+	uptime
+	echo "Disk Space:"
+	df -h 
+} >> $log_file 
